af2a4313b8ddcfe5bcd20f5565d650b66064d23f
david.zaoui <david.zaoui@believe.com>
2021-07-26 23:56:00 +0200

    first commit

---
+++ conf/mariadb/scripts/init.sql
@@ -0,0 +1,15 @@
+/*
+CREATE DATABASE dockertest 
+CHARACTER SET = 'utf8' 
+COLLATE = 'utf8_general_ci';
+
+USE dockertest;
+
+CREATE TABLE employees (
+first_name varchar(25),
+last_name  varchar(25),
+department varchar(15),
+email  varchar(50)
+);
+
+*/
